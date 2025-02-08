fc8b79f41e68652b03d2d11597a091fa863c4595
Igor <igoroliveira0150@gmail.com>
2025-02-08 12:35:26 -0300

    Online

--- fram-artesanato/backend/model/index.js
+++ fram-artesanato/backend/model/index.js
@@ -4,10 +4,7 @@ const express = require("express");
 const app = express();
 const porta = process.env.PORT || 3000;
 
-const allowedOrigins = [
-  "https://fram-artesanatos-frontend.vercel.app",
-  "http://localhost:4000"
-];
+const allowedOrigins = ["https://fram-artesanatos-frontend.vercel.app"];
 
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", allowedOrigins[0]);
