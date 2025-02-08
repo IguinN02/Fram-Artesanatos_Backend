095752aeb0e0ef661dc73f4ef19b2e0c0ce9ae3e
Igor <igoroliveira0150@gmail.com>
2025-02-08 16:53:23 -0300

    Descrição da alteração

--- fram-artesanato/backend/model/usuario.js
+++ fram-artesanato/backend/model/usuario.js
@@ -92,7 +92,7 @@ const verificarToken = (req, res, next) => {
   });
 };
 
-usuarioRouter.get("/Perfil", verificarToken, (req, res) => {
+usuarioRouter.get("/perfil", verificarToken, (req, res) => {
   db.query("SELECT id, nome, email, telefone FROM usuarios WHERE id = $1", [req.usuarioId])
     .then((results) => {
       if (results.rows.length === 0) {
