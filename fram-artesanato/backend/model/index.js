dd1b8d7b146b95a00314c6f26b1d29ffb4530723
Igor <igoroliveira0150@gmail.com>
2025-02-08 12:27:08 -0300

    Cadastro-completo

--- fram-artesanato/backend/model/index.js
+++ fram-artesanato/backend/model/index.js
@@ -4,16 +4,16 @@ const express = require("express");
 const app = express();
 const porta = process.env.PORT || 3000;
 
-const allowedOrigins = ["https://fram-artesanatos-frontend.vercel.app"];
+const allowedOrigins = ["https://fram-artesanatos-frontend.vercel.app", "http://localhost:4000"];
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", allowedOrigins[0]); 
+  res.header("Access-Control-Allow-Origin", allowedOrigins[0]);
   res.header("Access-Control-Allow-Credentials", "true");
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
   res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
 
   if (req.method === "OPTIONS") {
-    return res.sendStatus(204); 
+    return res.sendStatus(204);
   }
 
   next();
